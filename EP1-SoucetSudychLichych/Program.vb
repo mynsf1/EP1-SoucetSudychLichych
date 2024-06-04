6a3e7aaa45a6766be372b16f4981c6550709cecb
Kateryna Rubchevska <Rubchevska.Kateryna@stud.vassboskovice.cz>
2024-06-04 09:25:50 +0200

    v1

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -1,7 +1,22 @@
 Imports System
 
 Module Program
-  Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
+    Sub Main(args As String())
+        Dim pocetcisel As Integer
+        Dim cislo As String
+
+
+        Console.WriteLine(" Zadejte poèet èísel:")
+        Console.ReadLine()
+
+        If Integer.TryParse(cislo, pocetcisel) Then
+
+            For i As Integer = 1 To pocetcisel
+                Console.WriteLine($" Zadejte èíslo {i}:")
+                cislo = Console.ReadLine
+
+            Next
+
+        End If
+    End Sub
 End Module
