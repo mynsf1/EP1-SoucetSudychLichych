009fed8b285fdec00eaaa1b38aa16583484147e7
Kateryna Rubchevska <Rubchevska.Kateryna@stud.vassboskovice.cz>
2024-06-12 21:26:02 +0200

    idk

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -1,24 +1,49 @@
 Imports System
 
 Module Program
-    Sub Main(args As String())
-        Dim pocetcisel As Integer
-        Dim cislo As String
-
-
-        Console.WriteLine(" Zadejte poèet èísel:")
-        Console.ReadLine()
-
-        If Integer.TryParse(cislo, pocetcisel) Then
-
-            For i As Integer = 1 To pocetcisel
-                Console.WriteLine($" Zadejte èíslo {i}:")
-                cislo = Console.ReadLine
-
-            Next
-
-        Else
-            Console.WriteLine("Neplatná hodnota. Zadejte prosím celé èíslo.")
+    Sub Main()
+        Dim pocetcisel As Object
+        Console.Clear()
+        Console.ForegroundColor = ConsoleColor.White
+        Console.Write("Ahoj, kolik Ÿ¡sel chceç? ")
+        Console.WriteLine()
+        pocetcisel = Console.ReadLine
+        Console.WriteLine()
+        If IsNumeric(pocetcisel) = False Then
+            'zde zjiçóujeme zdali je nebo nen¡ v objektu
+            'svov no Ÿ¡slo
+            Console.ForegroundColor = ConsoleColor.Red
+            Console.WriteLine("Chyba - nezadali jste Ÿ¡slo! ")
+            Console.WriteLine()
+            Console.ReadKey()
+            Main()
         End If
+        Dim cistecislo As Integer = pocetcisel
+        If cistecislo <= 0 Then
+            'zde zjiçóujeme je Ÿ¡slo nula
+            'a mebo z porn‚
+            Console.ForegroundColor = ConsoleColor.Red
+            Console.WriteLine("Chyba - nezadali jste kladn‚ Ÿ¡slo! ")
+            Console.WriteLine()
+            Console.ReadKey()
+            Main()
+        End If
+        Dim nahoda As Integer
+        Dim soucetsudych, soucetlychych As Double
+        Console.Clear()
+        For x = 1 To cistecislo
+            nahoda = Rnd() * 1000
+            Console.WriteLine(nahoda)
+            If nahoda Mod 2 = 0 Then
+                soucetsudych += nahoda
+            Else
+                soucetlychych += nahoda
+            End If
+        Next
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine("SouŸet sudìch je: " & soucetsudych)
+        Console.WriteLine("SouŸet lychìch je: " & soucetlychych)
+        Console.ReadKey()
+
     End Sub
 End Module
